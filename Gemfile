a2c51572a77790daf2c416cea4e92b76c026306b
Matt D <MDes41@users.noreply.github.com>
2016-12-06 11:47:41 -0700

    Got tweets from feed

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'figaro'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'stream_rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 	def index
+		client = Stream::Client.new(ENV['STREAM_API_KEY'], ENV['STREAM_API_SECRET'])
+		# For the feed group 'user' and user id 'eric' get the feed
+		current_user_feed = client.feed('user', current_user.username)
+		# Add the activity to the feed
+		current_user.tweets.each do |tweet|
+			activity_data = {:actor => current_user.username, :verb => 'tweet', :object => tweet.id }
+			activity_response = current_user_feed.add_activity(activity_data)
+		end
 
+		feed = StreamRails.feed_manager.get_user_feed(current_user.username)
+    @tweets = feed.get()['results'].map do |feed|
+    	Tweet.find(feed['object'])
+    end
+
+    # byebug
+    # @activities = @enricher.enrich_activities(results)
 	end
 end
